0e5db0568ffb0d24eea29d923fb857b92acd5bc0
tyler fretz <fretztyler@gmail.com>
2021-07-14 14:27:00 -0400

    add comments to mobile

--- client/src/pages/SingleRecipe/MobileRecipe.js
+++ client/src/pages/SingleRecipe/MobileRecipe.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
 
 import RecipeMediaMobile from './RecipeMediaMobile';
 import ContentInnerMobile from './ContentInnerMobile';
 import RecipeFullStats from './RecipeFullStats';
 import RelatedSideBar from './RelatedSideBar';
+import CommentForm from './CommentForm';
+import CommentList from './CommentList';
 
 const useStyles = makeStyles({
 	mobileContainer: {
@@ -27,10 +28,20 @@ const useStyles = makeStyles({
 		padding: 0,
 		boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.1)',
 	},
+	commentSection: {
+		maxWidth: 360,
+		padding: '5%',
+		background: '#FFF',
+		boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.1)',
+		borderRadius: '3px',
+	}
 });
 
-const MobileRecipe = ({ recipe, handleVote }) => {
+const MobileRecipe = ({ recipe, handleVote, handleAddComment, handleSave }) => {
 	const classes = useStyles();
+	const commentRef = useRef(null);
+
+	const executeCommentScroll = () => commentRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
 	return (
 		<Container className={classes.mobileContainer}>
@@ -40,6 +51,12 @@ const MobileRecipe = ({ recipe, handleVote }) => {
 						thumbImageUrl={recipe.thumbImageUrl}
 						youtubeUrl={recipe.youtubeUrl}
 						name={recipe.name}
+						commentsLength={recipe.comments.length}
+						upvoteCount={recipe.upvoteCount}
+						handleVote={handleVote}
+						handleSave={handleSave}
+						handleAddComment={handleAddComment}
+						executeCommentScroll={executeCommentScroll}
 					/>
 					<ContentInnerMobile
 						name={recipe.name}
@@ -55,7 +72,10 @@ const MobileRecipe = ({ recipe, handleVote }) => {
 			<Container className={classes.sectionContainer}>
 				<RelatedSideBar category={recipe.category} />
 			</Container>
-			<Button onClick={handleVote}>Vote</Button>
+			<Container className={classes.commentSection} ref={commentRef}>
+				<CommentList comments={recipe.comments} />
+				<CommentForm handleAddComment={handleAddComment} />
+			</Container>
 		</Container>
 	);
 };
--- client/src/pages/SingleRecipe/RecipeMediaMobile.js
+++ client/src/pages/SingleRecipe/RecipeMediaMobile.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import IconButton from '@material-ui/core/IconButton';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ModeCommentIcon from '@material-ui/icons/ModeComment';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 import noVideo from '../../assets/noVideo.jpg';
 
@@ -36,9 +41,23 @@ const useStyles = makeStyles({
 		width: '100%',
 		height: '100%',
 	},
+	metaCount: {
+		display: 'flex',
+		justifyContent: 'space-around',
+		marginTop: '5%'
+	}
 });
 
-const RecipeMediaMobile = ({ youtubeUrl, thumbImageUrl, name }) => {
+const RecipeMediaMobile = ({
+	youtubeUrl,
+	thumbImageUrl,
+	name,
+	commentsLength,
+	upvoteCount,
+	handleSave,
+	handleVote,
+	executeCommentScroll
+}) => {
 	const classes = useStyles();
 	const [mediaUrl, setMediaUrl] = useState(thumbImageUrl);
 
@@ -89,6 +108,25 @@ const RecipeMediaMobile = ({ youtubeUrl, thumbImageUrl, name }) => {
 					</Button>
 					<Button onClick={() => setMediaUrl(videoId)}>Video</Button>
 				</ButtonGroup>
+			</Container>
+			<Container className={classes.metaCount}>
+				<div>
+					<IconButton onClick={() => handleSave()} color='secondary'>
+						<FavoriteIcon />
+					</IconButton>
+				</div>
+				<div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+					<IconButton onClick={() => handleVote()} color='secondary'>
+						<ThumbUpAltIcon />
+					</IconButton>
+					<Typography>{upvoteCount}</Typography>
+				</div>
+				<div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+					<IconButton onClick={() => executeCommentScroll()} color='secondary'>
+						<ModeCommentIcon />
+					</IconButton>
+					<Typography>{commentsLength}</Typography>
+				</div>
 			</Container>
 		</>
 	);
